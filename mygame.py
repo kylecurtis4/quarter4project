8816b4ee1267eaa1ae09645b27db09b1224d65b3
kylecurtis4 <166721120+kylecurtis4@users.noreply.github.com>
2024-04-12 09:40:54 -0400

    Add files via upload

--- mygame.py
+++ mygame.py
@@ -32,6 +32,15 @@ class Player(pygame.sprite.Sprite):
         self.surf = pygame.Surface((75, 25))
         self.surf.fill((255, 255, 255))
         self.rect = self.surf.get_rect()
+        def update(self,pressed_keys):
+            if pressed_keys[K_UP]:
+                self.rect.move_ip(0,-5)
+            if pressed_keys[K_DOWN]:
+                self.rect.move_ip(0,5)
+            if pressed_keys[K_RIGHT]:
+                self.rect.move_ip(-5,0)
+            if pressed_keys[K_LEFT]:
+                self.rect.move_ip(5,0)
 
 # Initialize pygame
 pygame.init()
@@ -58,12 +67,14 @@ while running:
         # Check for QUIT event. If QUIT, then set running to false.
         elif event.type == QUIT:
             running = False
+        pressed_keys = pygame.key.get_pressed()
+        player.update(pressed_keys)
 
     # Fill the screen with black
     screen.fill((0, 0, 0))
 
     # Draw the player on the screen
-    screen.blit(player.surf, player.rect )
+    screen.blit(player.surf, (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
 
     # Update the display
     pygame.display.flip()
