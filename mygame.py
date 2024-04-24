da2806df3089fc5d36ea7d9beedf3fde5ca158d3
kylecurtis4 <166721120+kylecurtis4@users.noreply.github.com>
2024-04-24 09:17:31 -0400

    Add files via upload

--- mygame.py
+++ mygame.py
@@ -19,7 +19,7 @@ from pygame.locals import (
     KEYDOWN,
     QUIT,
 )
-
+import random
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -32,7 +32,7 @@ class Player(pygame.sprite.Sprite):
         self.surf.fill((255, 255, 255))
         self.rect = self.surf.get_rect()
     def update(self,pressed_keys):
-        if pressed_keys[K_UP]:
+        if pressed_keys[K_UP]:  
             self.rect.move_ip(0,-5)
             print(self.rect.x, self.rect.y)
         if pressed_keys[K_DOWN]:
@@ -41,12 +41,43 @@ class Player(pygame.sprite.Sprite):
             self.rect.move_ip(5,0)
         if pressed_keys[K_LEFT]:
             self.rect.move_ip(-5,0)
-
-
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        if self.rect.bottom >= SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Enemy, self).__init__()
+        self.surf = pygame.Surface((20,10))
+        self.surf.fill((255,255,255))
+        self.rect = self.surf.get_rect(
+            center=(
+                  random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100), random.randint(0,SCREEN_HEIGHT)  
+                )
+            )  
+        self.speed = random.randint(5,20)
+    def update(self):
+        self.rect.move_ip(-self.speed, 0)
+        if self.rect.right < 0:
+            self.kill()
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+ADDENEMY = pygame.USEREVENT + 1
+pygame.time.set_timer(ADDENEMY, 250)
+
 player = Player()
+
+enemies = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
+
 running = True
+
 
 while running:  
     for event in pygame.event.get():
@@ -60,7 +91,8 @@ while running:
 
     screen.fill((0, 0, 0))
     
-    screen.blit(player.surf, (player.rect.x, player.rect.y))
+    for entity in all_sprites:
+        screen.blit(entity.surf, entity.rect)
 
     pygame.display.flip()
 
