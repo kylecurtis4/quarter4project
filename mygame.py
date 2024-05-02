ccb4ca4ac5b1245c5f62ce455062620d47176fd0
kylecurtis4 <166721120+kylecurtis4@users.noreply.github.com>
2024-05-02 09:41:55 -0400

    Add files via upload

--- mygame.py
+++ mygame.py
@@ -20,6 +20,7 @@ from pygame.locals import (
     K_RIGHT,
     K_ESCAPE,
     KEYDOWN,
+    K_SPACE,
     QUIT,
 )
      
@@ -56,7 +57,7 @@ class Player(pygame.sprite.Sprite):
 class Enemy(pygame.sprite.Sprite):
     def __init__(self):
         super(Enemy, self).__init__()
-        self.surf = pygame.image.load("missile.png").convert()
+        self.surf = pygame.image.load("mymissile.png").convert()
         self.surf.set_colorkey((255,255,255), RLEACCEL)
         self.rect = self.surf.get_rect(
             center=(
@@ -75,13 +76,14 @@ player_score = 0
 pygame.init()       
 
 myfont = pygame.font.SysFont("monospace", 15)
+gameoverfont = pygame.font.SysFont("Arial", 35, bold = True, )
 
 white = (255,255,255)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+# Spawns Enemies
 ADDENEMY = pygame.USEREVENT + 1
-pygame.time.set_timer(ADDENEMY, 300)
+pygame.time.set_timer(ADDENEMY, (1000 - (total_time * 2)))
 
 player = Player()
 enemy = Enemy()
@@ -89,20 +91,32 @@ enemies = pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
 all_sprites.add(player)
 
-running = True
 
 clock = pygame.time.Clock()
-
-timer_interval = 1000 # 1 seconds
+# Sets a timer for updating score and total time
+timer_interval = 100
 timer_event = pygame.USEREVENT + 2
 pygame.time.set_timer(timer_event, timer_interval)    
 
-
+running = True
+alive = True 
 while running:  
+    
+    # Process events 
     for event in pygame.event.get():
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE:
                 running = False
+            elif event.key == K_SPACE and not alive:
+                # RESTART the game
+                player = Player()
+                enemy = Enemy()
+                enemies = pygame.sprite.Group()
+                all_sprites = pygame.sprite.Group()
+                all_sprites.add(player)
+                alive = True
+                player_score = 0 
+                total_time = 15 
         elif event.type == QUIT:
             running = False
         elif event.type == ADDENEMY:
@@ -111,24 +125,38 @@ while running:
             all_sprites.add(new_enemy)
         elif event.type == timer_event:
              player_score += 1
-             total_time +=1
+             total_time +=0.1
         
-    pressed_keys = pygame.key.get_pressed()
-    player.update(pressed_keys) 
-    enemies.update()
-    text = "Score:" + str(player_score)
-    label = myfont.render(text, 1, white)
-    
-      
+         
+    if alive: 
+        # Updates Player and Enemies
+        pressed_keys = pygame.key.get_pressed()
+        player.update(pressed_keys) 
+        enemies.update()
+        # Draws the score 
+        text = "Score:" + str(player_score)
+        label = myfont.render(text, 1, white)    
+            
+        screen.fill((0, 0, 0))
+        screen.blit(label, (675, 15))
         
-    screen.fill((0, 0, 0))
-    screen.blit(label, (675, 15))
     
-    for entity in all_sprites:
-            screen.blit(entity.surf, entity.rect)
-    if pygame.sprite.spritecollideany(player, enemies):
-        player.kill()
-        running = False
+        # Draws the enemies 
+        for entity in all_sprites:
+                screen.blit(entity.surf, entity.rect)
+        # Collisions 
+        if pygame.sprite.spritecollideany(player, enemies):
+            player.kill()
+            game_over = "Game Over"
+            game_over_label = gameoverfont.render(game_over, 1 , white)
+            restart = "Space to Restart"
+            restart_label = myfont.render(restart, 1, white)
+            screen.fill((0,0,255))
+            screen.blit(game_over_label,((SCREEN_WIDTH/2),(SCREEN_HEIGHT/2)))
+            screen.blit(restart_label, (325, 300))
+            alive = False 
+    
+        
     
 
     pygame.display.flip()
